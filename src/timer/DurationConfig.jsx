1eeddfefad970c4d2abc01421eea527bda13af3e
Jorge Luis Shirai <jlshirai@gmail.com>
2017-11-03 12:52:41 -0300

    Add rc-time-picker & moment dependencies.

--- src/timer/DurationConfig.jsx
+++ src/timer/DurationConfig.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import moment from 'moment';
+
+import TimePicker from 'rc-time-picker';
+import 'rc-time-picker/assets/index.css';
 
 import {
   getSecsFieldFromDuration,
   getMinsFieldFromDuration,
   getHoursFieldFromDuration,
 } from './durationHelpers';
+
+const CLASSNAME_BASE = 'setup';
 
 /**
  * Configures the timer's duration, given an amount of hours, mins and secs.
@@ -20,61 +26,60 @@ class DurationConfig extends React.Component {
       minsField: getMinsFieldFromDuration(props.duration),
       hoursField: getHoursFieldFromDuration(props.duration),
     };
+    this.updateHours = this.updateHours.bind(this);
+    this.updateMins = this.updateMins.bind(this);
+    this.updateSecs = this.updateSecs.bind(this);
     this.updateDuration = this.updateDuration.bind(this);
   }
 
-  updateDuration(evt) {
-    let { hoursField, minsField, secsField } = this.state;
+  updateHours(time) {
+    const { minsField, secsField } = this.state;
+    const hoursField = time.hour();
+    this.setState({ hoursField });
+    this.updateDuration(hoursField, minsField, secsField);
+  }
 
-    const fieldDuration = Number.parseInt(evt.target.value, 10);
-    switch (evt.target.name) {
-      case 'hours':
-        this.setState({ hoursField: fieldDuration });
-        hoursField = fieldDuration;
-        break;
-      case 'mins':
-        this.setState({ minsField: fieldDuration });
-        minsField = fieldDuration;
-        break;
-      default:
-        this.setState({ secsField: fieldDuration });
-        secsField = fieldDuration;
-    }
+  updateMins(time) {
+    const { hoursField, secsField } = this.state;
+    const minsField = time.minute();
+    this.setState({ minsField });
+    this.updateDuration(hoursField, minsField, secsField);
+  }
 
+  updateSecs(time) {
+    const { hoursField, minsField } = this.state;
+    const secsField = time.second();
+    this.setState({ secsField });
+    this.updateDuration(hoursField, minsField, secsField);
+  }
+
+  updateDuration(hoursField, minsField, secsField) {
     const duration = (hoursField * 3600) + (minsField * 60) + secsField;
     this.props.onDurationChange(duration);
   }
 
   render() {
+    const { hoursField, minsField, secsField } = this.state;
+
     return (
-      <div>
-        <input
-          name="hours"
-          type="number"
-          value={this.state.hoursField}
-          onChange={this.updateDuration}
-          min="0"
-          step="1"
+      <div className={CLASSNAME_BASE}>
+        <TimePicker
+          value={moment().hour(hoursField)}
+          showMinute={false}
+          showSecond={false}
+          onChange={this.updateHours}
         />
-        :
-        <input
-          name="mins"
-          type="number"
-          value={this.state.minsField}
-          onChange={this.updateDuration}
-          min="0"
-          max="59"
-          step="1"
+        <TimePicker
+          value={moment().minute(minsField)}
+          showHour={false}
+          showSecond={false}
+          onChange={this.updateMins}
         />
-        :
-        <input
-          name="secs"
-          type="number"
-          value={this.state.secsField}
-          onChange={this.updateDuration}
-          min="0"
-          max="59"
-          step="1"
+        <TimePicker
+          value={moment().second(secsField)}
+          showHour={false}
+          showMinute={false}
+          onChange={this.updateSecs}
         />
       </div>
     );
