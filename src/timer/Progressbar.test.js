1b1ef5d389ad411cc0428550763734877fa732c7
Jorge Luis Shirai <jlshirai@gmail.com>
2017-11-20 17:09:06 -0300

    Add basic jest tests.
    Add basic tests with enzyme and react-test-renderer.

---
+++ src/timer/Progressbar.test.js
@@ -0,0 +1,37 @@
+import renderer from 'react-test-renderer';
+
+import { Progressbar } from './Progressbar';
+
+const DEFAULT_PROPS = {
+  duration: 4000,
+  remainingTime: 4000,
+  isTicking: false,
+};
+
+test('class in SVG changes when remainingTime is zero and isTicking is truthy', () => {
+  const props = {
+    ...DEFAULT_PROPS,
+  };
+
+  let component = renderer.create(Progressbar(props));
+  expect(component.toJSON()).toMatchSnapshot();
+
+  props.remainingTime = 2000;
+  props.isTicking = true;
+  component = renderer.create(Progressbar(props));
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+
+test('class in SVG changes when remainingTime is zero', () => {
+  const props = {
+    ...DEFAULT_PROPS,
+  };
+
+  let component = renderer.create(Progressbar(props));
+  expect(component.toJSON()).toMatchSnapshot();
+
+  props.remainingTime = 0;
+  component = renderer.create(Progressbar(props));
+  expect(component.toJSON()).toMatchSnapshot();
+});
---
+++ src/timer/StartReset.test.js
@@ -0,0 +1,46 @@
+import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
+
+import { StartReset } from './StartReset';
+
+const DEFAULT_PROPS = {
+  isDurationConfigured: false,
+  onStart: () => {},
+  onReset: () => {},
+}
+
+test('Button changes from Start to Reset after duration is configured', () => {
+  const props = {
+    ...DEFAULT_PROPS
+  };
+
+  let component = renderer.create(StartReset(props));
+  expect(component.toJSON()).toMatchSnapshot();
+
+  props.isDurationConfigured = true;
+  component = renderer.create(StartReset(props));
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+test('When duration is not configured, click trigger onStart', () => {
+  const props = {
+    ...DEFAULT_PROPS,
+    onStart: jest.fn(),
+  };
+
+  const component = shallow(StartReset(props));
+  component.find('button').simulate('click');
+  expect(props.onStart).toHaveBeenCalled();
+});
+
+test('When duration is configured, click trigger onReset', () => {
+  const props = {
+    ...DEFAULT_PROPS,
+    isDurationConfigured: true,
+    onReset: jest.fn(),
+  };
+
+  const component = shallow(StartReset(props));
+  component.find('button').simulate('click');
+  expect(props.onReset).toHaveBeenCalled();
+});
